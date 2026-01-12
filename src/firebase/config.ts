14e375ef284051c16599e8b3eeb38cb42200eb10
Thimira Vishwa <vishwathimira2@gmail.com>
2026-01-12 12:27:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9035200826-5d7a4",
+  "appId": "1:136866783944:web:d06ed998399ffc41a53bfd",
+  "apiKey": "AIzaSyA_MY1aEbWL5EeHaCE_FEIrBtayCkGi-Xg",
+  "authDomain": "studio-9035200826-5d7a4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "136866783944"
+};
