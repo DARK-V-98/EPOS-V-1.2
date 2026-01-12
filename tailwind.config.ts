03f132bf932fb52235d805ad338b0ce9fcf4c535
Thimira Vishwa <vishwathimira2@gmail.com>
2026-01-12 13:23:39 +0000

    i cant acces the pages using menu

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -59,6 +59,10 @@ export default {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        sidebar: {
+          DEFAULT: "hsl(var(--sidebar-background))",
+          foreground: "hsl(var(--sidebar-foreground))",
+        },
       },
       borderRadius: {
         lg: "var(--radius)",
